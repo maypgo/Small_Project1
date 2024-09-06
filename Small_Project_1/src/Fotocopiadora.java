f2115791d3a55e131f71c22c6d1bb183e8a1aed6
maypgo <117177528+maypgo@users.noreply.github.com>
2024-09-05 19:55:00 -0500

    Add files via upload

---
+++ Small_Project_1/src/Fotocopiadora.java
@@ -0,0 +1,11 @@
+public class Fotocopiadora extends Servicio {
+    private int cantidadCopias;
+    private String tipoCopias; // "color" o "blanco y negro"
+
+    @Override
+    public double calcularCosto() {
+        // Implementa cálculo de costo basado en tipo de copia y cantidad
+    }
+
+    // Getters y Setters
+}
---
+++ Small_Project_1/src/Servicio.java
@@ -0,0 +1,10 @@
+public abstract class Servicio {
+    protected double valor;
+    protected String fecha;
+
+    public abstract double calcularCosto();
+
+    // Getters y Setters
+}
+
+
